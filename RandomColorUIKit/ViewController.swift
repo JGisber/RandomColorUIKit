45c7a363665afc29b4b6c05c000946a032ed7578
Josue Gisber <jgisber@gmail.com>
2020-10-01 22:31:43 -0600

    Initial Commit

---
+++ RandomColorUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColorUIKit
+//
+//  Created by Josue Gisber on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
